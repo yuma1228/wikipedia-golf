29759abe4da84b436124447a911bb5e0e3a24b50
yuma1228 <yuma0114n@gmail.com>
2026-02-10 17:52:57 +0900

    test

--- src/app/@modal/(.)preview/page.tsx
+++ src/app/@modal/(.)preview/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useRouter, useSearchParams } from "next/navigation";
 import { GoalModal } from "@/components/GoalModal";
 import { Suspense } from "react";
